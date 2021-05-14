73fa0fed26ee770a7dd7aa26b5474cbfb2e1b7a3
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2021-05-14 12:00:46 +0530

    Update songs.js

--- songs.js
+++ songs.js
@@ -757,7 +757,7 @@ var songs_playlist = [{
     subtitles: "Subs/songSubtitles.vtt",
     artist: "Hiroyuki Sawano"
   },
-   {
+  {
     name: "☆ Chouzetsu Dynamic",
     poster: "images/s1.jpg",
     src: "music/dbs/s1.mp3",
@@ -806,4 +806,95 @@ var songs_playlist = [{
     subtitles: "subs/songsubtitles.vtt",
     artist: "Akira Kushida "
   },
-];
+  {
+    name: "Haruka Mirai (Faraway Future)",
+    poster: "images/black-clover/o1.png",
+    src: "music/black-clover/o1.mp3",
+    subtitles: "",
+    artist: "Kankaku Piero"
+  },
+  {
+    name: "PAiNT it BLACK",
+    poster: "images/black-clover/o2.png",
+    src: "music/black-clover/o2.mp3",
+    subtitles: "",
+    artist: "BISH"
+  },
+  {
+    name: "Black Rover",
+    poster: "images/black-clover/o3.png",
+    src: "music/black-clover/o3.mp3",
+    subtitles: "",
+    artist: "Vickeblanka"
+  },
+  {
+    name: "Guess Who Is Back",
+    poster: "images/black-clover/o4.png",
+    src: "music/black-clover/o4.mp3",
+    subtitles: "",
+    artist: "Koda Kumi"
+  },
+  {
+    name: "Gamushara",
+    poster: "images/black-clover/o5.png",
+    src: "music/black-clover/o5.mp3",
+    subtitles: "",
+    artist: "Miyuna"
+  },
+  {
+    name: "Rakugaki Page (Graffiti Page)",
+    poster: "images/black-clover/o6.png",
+    src: "music/black-clover/o6.mp3",
+    subtitles: "",
+    artist: "Kankaku Piero"
+  },
+
+  {
+    name: "JUSTadICE",
+    poster: "images/black-clover/o7.png",
+    src: "music/black-clover/o7.mp3",
+    subtitles: "",
+    artist: "Seiko Oomori"
+  },
+  {
+    name: "sky & blue",
+    poster: "images/black-clover/o8.png",
+    src: "music/black-clover/o8.mp3",
+    subtitles: "",
+    artist: "GIRLFRIEND"
+  },
+  {
+    name: "RiGHT NOW",
+    poster: "images/black-clover/o9.png",
+    src: "music/black-clover/o9.mp3",
+    subtitles: "",
+    artist: "EMPiRE"
+  },
+  {
+    name: "Black Catcher",
+    poster: "images/black-clover/o10.png",
+    src: "music/black-clover/o10.mp3",
+    subtitles: "",
+    artist: "Vickeblanka"
+  },
+  {
+    name: "Stories",
+    poster: "images/black-clover/o11.png",
+    src: "music/black-clover/o11.mp3",
+    subtitles: "",
+    artist: "Snow Man"
+  },
+  {
+    name: "Everlasting Shine",
+    poster: "images/black-clover/o12.png",
+    src: "music/black-clover/o12.mp3",
+    subtitles: "",
+    artist: "TOMORROW X TOGETHER"
+  },
+  {
+    name: "Grandeur",
+    poster: "images/black-clover/o13.png",
+    src: "music/black-clover/o13.mp3",
+    subtitles: "",
+    artist: "Snow Man"
+  }]
