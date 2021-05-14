aaddf86a75d344ecd544dd16a6a1470ac1577e04
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2021-05-14 11:36:31 +0530

    Add files via upload

---
+++ playlists/playlist_6.js
@@ -0,0 +1,98 @@
+/*
+   Black clover
+*/
+
+ var myplaylist =[
+  {
+  name:   "Haruka Mirai (Faraway Future)" , 
+  poster: "images/black-clover/o1.png" , 
+  src :   "music/black-clover/o1.mp3" ,
+  subtitles : "" ,
+  artist: "Kankaku Piero"
+  },
+  {
+  name:   "PAiNT it BLACK" , 
+  poster: "images/black-clover/o2.png" , 
+  src :   "music/black-clover/o2.mp3" ,
+  subtitles : "" ,
+  artist: "BISH"
+  },
+  {
+  name:   "Black Rover" , 
+  poster: "images/black-clover/o3.png" , 
+  src :   "music/black-clover/o3.mp3" ,
+  subtitles : "" ,
+  artist: "Vickeblanka"
+  },
+  {
+  name:   "Guess Who Is Back" , 
+  poster: "images/black-clover/o4.png" , 
+  src :   "music/black-clover/o4.mp3" ,
+  subtitles : "" ,
+  artist: "Koda Kumi"
+  },
+  {
+  name:   "Gamushara" , 
+  poster: "images/black-clover/o5.png" , 
+  src :   "music/black-clover/o5.mp3" ,
+  subtitles : "" ,
+  artist: "Miyuna"
+  },
+  {
+  name:   "Rakugaki Page (Graffiti Page)" , 
+  poster: "images/black-clover/o6.png" , 
+  src :   "music/black-clover/o6.mp3" ,
+  subtitles : "" ,
+  artist: "Kankaku Piero"
+  },
+ 
+  {
+  name:   "JUSTadICE" , 
+  poster: "images/black-clover/o7.png" , 
+  src :   "music/black-clover/o7.mp3" ,
+  subtitles : "" ,
+  artist: "Seiko Oomori"
+  },
+  {
+  name:   "sky & blue" , 
+  poster: "images/black-clover/o8.png" , 
+  src :   "music/black-clover/o8.mp3" ,
+  subtitles : "" ,
+  artist: "GIRLFRIEND"
+  },
+  {
+  name:   "RiGHT NOW" , 
+  poster: "images/black-clover/o9.png" , 
+  src :   "music/black-clover/o9.mp3" ,
+  subtitles : "" ,
+  artist: "EMPiRE"
+  },
+  {
+  name:   "Black Catcher" , 
+  poster: "images/black-clover/o10.png" , 
+  src :   "music/black-clover/o10.mp3" ,
+  subtitles : "" ,
+  artist: "Vickeblanka"
+  },
+  {
+  name:   "Stories" , 
+  poster: "images/black-clover/o11.png" , 
+  src :   "music/black-clover/o11.mp3" ,
+  subtitles : "" ,
+  artist: "Snow Man"
+  },
+  {
+  name:   "Everlasting Shine" , 
+  poster: "images/black-clover/o12.png" , 
+  src :   "music/black-clover/o12.mp3" ,
+  subtitles : "" ,
+  artist: "TOMORROW X TOGETHER"
+  },
+  {
+  name:   "Grandeur" , 
+  poster: "images/black-clover/o13.png" , 
+  src :   "music/black-clover/o13.mp3" ,
+  subtitles : "" ,
+  artist: "Snow Man"
+  },
+]
