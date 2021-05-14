68790ea3ed16df3df86ce5f749ece55c20485698
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2021-05-15 01:11:02 +0530

    Update playlist_0.js

--- playlists/playlist_0.js
+++ playlists/playlist_0.js
@@ -1,5 +1,5 @@
 /*
-Anime 
+  My Hero Academia 
 */
 
 var myplaylist =[
@@ -7,126 +7,126 @@ var myplaylist =[
   name:   "The Day" , 
   poster: "images/myhero1.jpg" , 
   src :   "music/myhero1.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Porno Graffitti"
   },
   {
   name:   "HEROES" , 
   poster: "images/myheroend1.jpg" , 
   src :   "music/myheroend1.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Brian the Sun"
   },
   {
   name:   "Peace Sign" , 
   poster: "images/myhero2.jpg" , 
   src :   "music/myhero2.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Kenshi Yonezu"
   },
   {
   name:   "Dakara, Hitori ja nai" , 
   poster: "images/myheroend2.jpg" , 
   src :   "music/myheroend2.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Little Glee Monster"
   },
   {
   name:   "Sora ni Utaeba" , 
   poster: "images/myhero3.jpg" , 
   src :   "music/myhero3.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Amazarashi"
   },
   {
   name:   "Datte Atashi no Hero" , 
   poster: "images/myheroend3.jpg" , 
   src :   "music/myheroend3.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "LiSA"
   },
   {
   name:   "Odd Future" , 
   poster: "images/myhero4.jpg" , 
   src :   "music/myhero4.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "UVERworld"
   },
   {
   name:   "Update" , 
   poster: "images/myheroend4.jpg" , 
   src :   "music/myheroend4.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Miwa"
   },
   {
   name:   "Make my story" , 
   poster: "images/myhero5.jpg" , 
   src :   "music/myhero5.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Lenny code fiction"
   },
   {
   name:   "Long Hope Philia" , 
   poster: "images/myheroend5.jpg" , 
   src :   "music/myheroend5.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Masaki Suda"
   },
   {
   name:   "Polaris" , 
   poster: "images/myhero6.jpg" , 
   src :   "music/myhero6.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Blue Encount"
   },
   {
   name:   "Kokai no Uta" , 
   poster: "images/myheroend6.jpg" , 
   src :   "music/myheroend6.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Sayuri"
   },
   {
   name:   "Starmarker" , 
   poster: "images/myhero7.jpg" , 
   src :   "music/myhero7.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "KANA-BOON"
   },
   {
   name:   "Shout Baby" , 
   poster: "images/myheroend7.jpg" , 
   src :   "music/myheroend7.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Ryokuoshoku Shakai"
   },
   {
   name:   "MHA (OST)" , 
   poster: "images/myheroost1.jpg" , 
   src :   "music/myheroost1.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Yuki Hayashi"
   },
   {
   name:   "HERO A" , 
   poster: "images/myheroost2.jpg" , 
   src :   "music/myheroost2.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Yuki Hayashi"
   },
   {
   name:   "Just Another Hero" , 
   poster: "images/myheroost2.jpg" , 
   src :   "music/myheroost3.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Yuki Hayashi"
   },
   {
   name:   "Might+U" , 
   poster: "images/myheroost3.jpg" , 
   src :   "music/myheroost4.mp3" ,
-  subtitles : "Subs/" ,
+  subtitles : "subs/empty.vtt" ,
   artist: "Makayla Phillips"
   },
   {
