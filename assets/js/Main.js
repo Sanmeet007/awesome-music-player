84ac381958e5ea60166242ffc741dbb646876ef2
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2021-05-16 20:46:46 +0530

    Update Main.js

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -1,6 +1,6 @@
 //   var darkMode = false;
 //iam = true;
-
+let is_playlist = false;
 var count = 0;
 var shuffled = false;
 var giveSong, giveArtist, givePoster, now_poster;
@@ -37,9 +37,13 @@ function loadSongs(id) {
        block: "center"
      });
      */
-    element.parentNode.scrollTop = element.offsetTop - 120;
-
-    //   element.scrollTop = "0";
+     if(!is_playlist){
+    // For Playlist one 
+    element.parentElement.scrollTop =   element.offsetTop - 120;
+     }else{
+    // For normal One 
+     element.parentElement.parentElement.scrollTop = element.offsetTop - 120;
+     }
   }catch(e) {
     // console.log(e)
   }
@@ -200,16 +204,16 @@ $('#icon_2').on('click', function () {
 
     },
     error: function() {
-        document.querySelector('.ChangeDiv').innerHTML = `
-        <div class="noResponse"> 
-        <img src="images/error.png" alt=""  class="error"/><span>
-        <i class="loser fa fa-frown-o" aria-hidden="true"></i>
-        Aw Snap Something Went Wrong
-        <br></br> 
-        <button onclick="tryReloading('controls')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
-       </span>
-       
-        </div>`
+      document.querySelector('.ChangeDiv').innerHTML = `
+      <div class="noResponse">
+      <img src="images/error.png" alt=""  class="error"/><span>
+      <i class="loser fa fa-frown-o" aria-hidden="true"></i>
+      Aw Snap Something Went Wrong
+      <br></br>
+      <button onclick="tryReloading('controls')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
+      </span>
+
+      </div>`
     }
 
   });
@@ -222,6 +226,7 @@ $('#icon_1').on('click', function () {
     url: "home.txt",
     type: "get",
     beforeSend: function () {
+      is_playlist = false;
       $('.ChangeDiv').html(divHtml);
       showLoader();
     },
@@ -241,16 +246,16 @@ $('#icon_1').on('click', function () {
       element.parentNode.scrollTop = element.offsetTop  - 120;
     },
     error: function() {
-       document.querySelector('.ChangeDiv').innerHTML = `
-        <div class="noResponse"> 
-        <img src="images/error.png" alt=""  class="error"/><span>
-        <i class="loser fa fa-frown-o" aria-hidden="true"></i>
-        Aw Snap Something Went Wrong
-        <br></br> 
-        <button onclick="tryReloading('home')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
-       </span>
-       
-        </div>`
+      document.querySelector('.ChangeDiv').innerHTML = `
+      <div class="noResponse">
+      <img src="images/error.png" alt=""  class="error"/><span>
+      <i class="loser fa fa-frown-o" aria-hidden="true"></i>
+      Aw Snap Something Went Wrong
+      <br></br>
+      <button onclick="tryReloading('home')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
+      </span>
+
+      </div>`
     }
 
   });
@@ -266,6 +271,7 @@ $('#icon_3').on('click', function () {
     url: "playlist.txt",
     type: "get",
     beforeSend: function () {
+      is_playlist = true;
       $('.ChangeDiv').html(divHtml);
       showLoader();
     },
@@ -276,16 +282,16 @@ $('#icon_3').on('click', function () {
 
     },
     error: function() {
-       document.querySelector('.ChangeDiv').innerHTML = `
-        <div class="noResponse"> 
-        <img src="images/error.png" alt=""  class="error"/><span>
-        <i class="loser fa fa-frown-o" aria-hidden="true"></i>
-        Aw Snap Something Went Wrong
-        <br></br> 
-        <button onclick="tryReloading('playlist')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
-       </span>
-       
-        </div>`
+      document.querySelector('.ChangeDiv').innerHTML = `
+      <div class="noResponse">
+      <img src="images/error.png" alt=""  class="error"/><span>
+      <i class="loser fa fa-frown-o" aria-hidden="true"></i>
+      Aw Snap Something Went Wrong
+      <br></br>
+      <button onclick="tryReloading('playlist')"><i style="margin-right:5px" class="fas fa-redo"></i> Try Again </button>
+      </span>
+
+      </div>`
     }
 
   });
@@ -334,10 +340,12 @@ function search_toggle(id) {
   back.click();
   try {
     element = $('.playview')[id];
-    element.scrollIntoView({
+    //console.log(element)
+    element.parentNode.scrollTop = element.offsetTop  - 120;
+  /*  element.scrollIntoView({
       behavior: "smooth",
       block: "center"
-    });
+    });*/
   }catch(e) {
     // console.log(e)
   }
@@ -345,6 +353,7 @@ function search_toggle(id) {
 // For Song Toggle
 function toogle_function(id) {
   loadSongs(id);
+
   $('.song').removeClass('active');
   $('#'+id).addClass('active');
   var playMusicBtn = document.getElementById('play_pause')
@@ -456,13 +465,13 @@ function togglePlay() {
   } else {
     $('#play_pause').html(' <i class="fa fa-play" aria-hidden="true"></i>')
     // alert('playing')
-  //  music.pause()
- if (playPromise !== undefined) {
-    playPromise.then(_ => {
-       music.pause()
-    })
-    .catch(error => { });
-  }
+    //  music.pause()
+    if (playPromise !== undefined) {
+      playPromise.then(_ => {
+        music.pause()
+      })
+      .catch(error => {});
+    }
   }
 
 }
@@ -472,7 +481,6 @@ $(document).ready(function() {
   var get_id = $('#get_id').html();
   $('.song').removeClass("active")
   $('#'+get_id).addClass("active")
-
 });
 
 document.addEventListener('click', ()=> {
@@ -496,6 +504,7 @@ var next_song = document.getElementById('next');
 var previous_song = document.getElementById('previous');
 
 next_song.onclick = function() {
+
   var get_id = document.getElementById('get_id');
   get_id = get_id.innerHTML;
 
@@ -643,35 +652,290 @@ function hideLoader() {
 }
 
 
-function loadeddata(){
- $('.absolute').css('visibility' , 'hidden')
+function loadeddata() {
+  $('.absolute').css('visibility',
+    'hidden')
 }
-function loadstart(){
- $('.absolute').css('visibility' , 'visible')
+function loadstart() {
+  $('.absolute').css('visibility',
+    'visible')
 }
-function waiting (){
- $('.absolute').css('visibility' , 'visible')
+function waiting () {
+  $('.absolute').css('visibility',
+    'visible')
 }
-function isplaying(){
- $('.absolute').css('visibility' , 'hidden')
+function isplaying() {
+  $('.absolute').css('visibility',
+    'hidden')
 }
 
-function tryReloading(route){
-  switch (route) {
-    case 'home':
-      $('#icon_1').click();
-      loadsongs(1)
-      break;
-      
-    case 'controls':
-       $('#icon_2').click()
-      break;
-      
-    case 'playlist':
-       $('#icon_3').click()
-      break;
-    
-    default:
-      // code
+
+var new_poster = '';
+var new_artist = "";
+var new_title = "";
+
+// # Light Mode
+
+$('#logo').on('click', function() {
+
+  var shuffling = Math.round(Math.random() * songs.length);
+  toogle_function(shuffling);
+});
+
+
+
+
+
+function toogle_replica (one) {
+  songs = myplaylist;
+  loadSongs(one);
+  var playMusicBtn = document.getElementById('play_pause')
+  playMusicBtn.click();
+}
+
+
+
+function mun() {
+
+  var tellMe = confirm("Do you want to reload ? ");
+
+  if (tellMe == true) {
+    songs = songs_playlist;
+
+    var icon1 = document.getElementById('icon_1')
+    icon1.click();
+    $('#get_id').text(" ");
+    $('.song').removeClass('active')
+
   }
+}
+
+var lol = document.getElementById('lol');
+
+if (music !== undefined) {
+  music.onplay = ()=> {
+    lol.click();
+  }
+  music.onpause = ()=> {
+    lol.click();
+  }
+}
+
+try {
+  navigator.mediaSession.setActionHandler('previoustrack', function() {
+    previous_song.click();
+  });
+
+
+  navigator.mediaSession.setActionHandler('nexttrack', function() {
+    next_song.click();
+  });
+  navigator.mediaSession.setActionHandler('seekbackward', function() {
+    backwardAudio();
+
+  });
+
+  navigator.mediaSession.setActionHandler('seekforward', function() {
+
+    forwardAudio();
+
+  });
+
+  navigator.mediaSession.setActionHandler('seekto', (details) => {
+    if (details.fastSeek && "fastSeek" in music) {
+      // Only use fast seek if supported.
+      music.fastSeek(details.seekTime);
+      return;
+    }
+    music.currentTime = details.seekTime;
+    // TODO: Update playback state.
+  });
+
+}
+
+// For advance MediaMetadata.album
+
+catch(e) {}
+
+
+
+// No db search
+$(document).ready(function() {
+  $('.fa-search').on('click', function() {
+    $('body').css('overflow', 'hidden')
+
+    $('.search').css('visibility', 'visible');
+    $('.search').css('opacity', '1');
+
+    setTimeout(function() {
+      $('input')[0].focus()
+    }, 100);
+
+  });
+
+
+
+  $('#search').on('keyup', function() {
+    $('.searchResults').html("");
+    var search = $(this).val();
+    var str, checkStr;
+    var i = 0;
+
+    while (i <= songs_playlist.length-1) {
+      str = songs_playlist[i].name;
+      checkStr = str.toLowerCase();
+      search = search.toLowerCase();
+
+      if (checkStr.includes(search) == true || checkStr.indexOf(search) != -1) {
+
+
+        $('.noResults').html("");
+
+        $('.searchResults').append('<div onclick="search_toggle('+i+')" class="search-result">'+str+'</div>');
+
+
+        if (search == "") {
+          $('.searchResults').html("")
+          $('.noResults').html('Search Your Favourite Music ')
+        }
+      }
+      i = i+1;
+    }
+  });
+
+  document.getElementById("search").addEventListener("search",
+    function(event) {
+      $(".searchResults").empty();
+      $('.noResults').html('Search Your Favourite Music ')
+    });
+
+
+
+});
+back.onclick = ()=> {
+  $('.search').css('visibility', 'hidden');
+  $('.search').css('opacity', '0');
+  $(".searchResults").empty();
+  $('#search').val("");
+  $('.noResults').html('Nothing To Display')
+}
+
+
+
+
+$(document).ready(function() {
+  if (darkMode == false) {
+    $('body').css('filter', 'invert(1)')
+    $('body').css('background', '#eaeaea')
+
+    //$('h3').css('color' , 'black')
+    $('img').css('filter', 'invert(1)')
+  }
+});
+
+
+function changeImg() {
+  $('img').css('filter',
+    'invert(1)')
+}
+
+function darkModeOff() {
+  $('body').css('filter',
+    'invert(1)')
+  $('img').css('filter',
+    'invert(1)')
+  $('body').css('background-color',
+    '#eaeaea')
+  $('.online').css('filter',
+    'invert(1)');
+  darkMode = false;
+
+}
+function darkModeOn() {
+  $('body').css('filter',
+    'invert(0)')
+  $('img').css('filter',
+    'invert(0)')
+  $('body').css('background-color',
+    '#1D1D1D');
+  $('.online').css('filter',
+    'invert(0)');
+  darkMode = true;
+
+}
+function turns() {
+  if (darkMode == false) {
+    darkModeOn()
+    $('#icon_4').html(`
+      <i class="ic fa fa-sun" aria-hidden="true"></i>
+      <div class="menu_class">
+      Mode
+      </div>
+      `);
+
+  } else {
+    darkModeOff()
+    $('#icon_4').html(`
+      <i class="ic fa fa-moon" aria-hidden="true"></i>
+      <div class="menu_class">
+      Mode
+      </div>
+      `);
+
+  }
+}
+
+if (navigator.onLine == false) {
+
+  $('.online').css('top', '0px');
+  $('.online').css('background', 'white');
+  $('.online').css('color', 'black');
+
+  $('.online').html('<i class="fas fa-exclamation-triangle"></i><span id="fontAwsome">  Please Check Your Connection</span>')
+  setTimeout(function() {
+    $('.online').css('top', '-21px')
+  }, 3500);
+}
+
+window.addEventListener('online', function() {
+
+
+  $('.online').css('top', '0px');
+  $('.online').css('background', 'green');
+  $('.online').css('color', 'white');
+  $('.online').html('<i class="fas fa-wifi"></i><span id="fontAwsome"> Back Online</span>')
+  setTimeout(function() {
+    $('.online').css('top', '-21px')
+  }, 3500);
+
+});
+
+window.addEventListener('offline', function() {
+
+
+  $('.online').css('top', '0px');
+  $('.online').css('background', 'white');
+  $('.online').css('color', 'black');
+
+  $('.online').html('<i class="fas fa-no-wifi"></i><span id="fontAwsome">  Please Check Your Connection</span>')
+  setTimeout(function() {
+    $('.online').css('top', '-21px')
+  }, 3500);
+});
+
+
+
+
+function hideMe() {
+  $('.new-modal').css('visibility', 'hidden');
+  $('.new-modal').css('opacity', '0');
+  $('.modal').css('visibility', 'hidden')
+  $('.modal').css('opacity', '0')
+
+}
+
+function showMe() {
+  $('.new-modal').css('visibility', 'visible');
+  $('.new-modal').css('opacity', '1');
+
 }
